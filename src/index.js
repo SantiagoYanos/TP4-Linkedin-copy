3b58ddf871c426aad2c5bf4c4ef3a3dda4d89dd6
Yanos <santiago.yanos@gmail.com>
2022-09-03 20:06:28 -0300

    Add: Database connection (PostgreSQL)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import app from './app'
-import https from 'https'
-import fs from 'fs'
+import app from "./app";
+import https from "https";
+import fs from "fs";
 
-https.createServer({
-    key: fs.readFileSync('./src/certs/key.pem'),
-    cert: fs.readFileSync('./src/certs/cert.pem')
-}, app).listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./src/certs/key.pem"),
+      cert: fs.readFileSync("./src/certs/cert.pem"),
+    },
+    app
+  )
+  .listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
