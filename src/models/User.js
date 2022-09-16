509abd554040fb0dd6db990d5bb6f31e76f94353
Yanos <santiago.yanos@gmail.com>
2022-09-16 20:58:37 -0300

    Fix: try & catch (User Model)

--- src/models/User.js
+++ src/models/User.js
@@ -2,57 +2,83 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function getAllUsers() {
-  const users = await prisma.user.findMany();
-  return users;
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function getOneUser(name) {
-  const user = await prisma.user.findUnique({
-    where: {
-      name: name,
-    },
-  });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        name: name,
+      },
+    });
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function createUser(data) {
-  const createdUser = await prisma.user.create({
-    data: data,
-  });
-  return createdUser;
+  try {
+    return user;
+    const createdUser = await prisma.user.create({
+      data: data,
+    });
+    return createdUser;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function updateUser(id, data) {
-  const updatedUser = await prisma.user.updateOne({
-    where: {
-      id: id,
-    },
-    data: data,
-  });
-  return updatedUser;
+  try {
+    const updatedUser = await prisma.user.updateOne({
+      where: {
+        id: id,
+      },
+      data: data,
+    });
+    return updatedUser;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function activeUser(id) {
-  const activeUser = await prisma.user.updateOne({
-    where: {
-      id: id,
-    },
-    data: {
-      active: true,
-    },
-  });
-  return activeUser;
+  try {
+    const activeUser = await prisma.user.updateOne({
+      where: {
+        id: id,
+      },
+      data: {
+        active: true,
+      },
+    });
+    return activeUser;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function deactiveUser(id) {
-  const deactiveUser = await prisma.user.updateOne({
-    where: {
-      id: id,
-    },
-    data: {
-      active: false,
-    },
-  });
-  return deactiveUser;
+  try {
+    const deactiveUser = await prisma.user.updateOne({
+      where: {
+        id: id,
+      },
+      data: {
+        active: false,
+      },
+    });
+    return deactiveUser;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export default {
