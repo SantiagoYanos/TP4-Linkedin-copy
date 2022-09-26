59dcd5751f77318b04f48da67aa24adaae53a825
Yanos <santiago.yanos@gmail.com>
2022-09-25 21:02:26 -0300

    Fix: Without certs

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import app from "../src/app.js";
 import https from "https";
 import fs from "fs";
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("./src/certs/key.pem"),
-      cert: fs.readFileSync("./src/certs/certificate.pem"),
-    },
-    app
-  )
-  .listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("./src/certs/key.pem"),
+//       cert: fs.readFileSync("./src/certs/certificate.pem"),
+//     },
+//     app
+//   )
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
